ec38f7a5b18eb08f88baad680ae2c65a5d6e130a
syed <syed.mehtab@yara.com>
2020-01-24 22:10:40 +0800

    PLotana conversation added

--- assets/js/home-bot.js
+++ assets/js/home-bot.js
@@ -2,11 +2,11 @@
 var homeBot = BotUI('home-demo');
 
 homeBot.message.add({
-  content: 'Hello there!'
+  content: 'Welcome to Plotana. I am here to provide you analytics of your dream plot.'
 }).then(function () {
   return homeBot.message.add({
     delay: 1500,
-    content: 'Wanna see a demo of what BotUI can do?'
+    content: 'Wanna see a demo of what Plotana can do?'
   });
 }).then(function () {
   return homeBot.action.button({
@@ -15,7 +15,7 @@ homeBot.message.add({
       text: 'Sure!',
       value: 'sure'
     }, {
-      text: 'Just skip to end 😒',
+      text: 'No',
       value: 'skip'
     }]
   });
@@ -32,50 +32,70 @@ homeBot.message.add({
 var tutorial = function () {
   homeBot.message.add({
     delay: 1000,
-    content: "Awesome! You can show buttons like the one you pressed."
+    content: "Are you planning to buy or sell?"
   }).then(function () {
-    return homeBot.message.add({
+    return homeBot.action.button({
       delay: 1000,
-      content: 'You can ask for text input like ..'
+      action: [{
+        text: 'Buy',
+        value: 'buy'
+      }, {
+        text: 'Sell',
+        value: 'sell'
+      }, {
+        text: 'I am poor',
+        value: 'nothing'
+      }]
     });
   }).then(function () {
     return homeBot.message.add({
-      delay: 1200,
-      content: 'Whats your name?'
+      delay: 1500,
+      content: 'Where is your plot?'
     });
   }).then(function () {
     return homeBot.action.text({
       delay: 800,
       action: {
-        value: 'John Doe',
+        value: 'BTM Layout',
         placeholder: 'Your name'
       }
     });
   }).then(function (res) {
     return homeBot.message.bot({
       delay: 500,
-      content: res.value + ' is a nice name!'
-    });
-  }).then(function (res) {
-    return homeBot.message.bot({
-      delay: 1400,
-      content: 'You can also embed something from 3rd party'
+      content: res.value + '. Aha, you are a rich fellow. Now please wait while I am pulling satellite images for analysis'
     });
   }).then(function (res) {
     return homeBot.message.add({
       delay: 1200,
       type: 'embed',
-      content: 'https://giphy.com/embed/v1PSPwbLIrata'
+      content: 'https://giphy.com/embed/HzMfJIkTZgx8s'
+    
+    });
+  }).then(function (res) {
+    return homeBot.message.bot({
+      delay: 4000,
+      content: 'Almost there.'
+    });
+  }).then(function (res) {
+    return homeBot.message.bot({
+      delay: 4000,
+      content: 'Here you go!'
+    });
+  }).then(function (res) {
+    return homeBot.message.bot({
+      delay: 4000,
+      content: 'There are 5000 new homes built in past 2 years around your plot with in the Radius of 2 Kilometers.'
     });
   }).then(function (res) {
     return homeBot.message.bot({
       delay: 2500,
-      content: 'There is a lot more you can do with BotUI'
+      content: 'The area is densely populated and expected to grow. Further, huge night light intensity detected, seems like people here are nocturnal.'
     });
   }).then(function (res) {
     return homeBot.message.bot({
       delay: 1000,
-      content: 'Now go on, explore the docs and build yourself a bot.'
+      content: 'Spread the word. Its coming soon.'
     });
   }).then(end);
 };
@@ -84,8 +104,9 @@ var end = function () {
   ga_record('message', 'end');
   homeBot.message.add({
     delay: 1000,
-    content: '!(book) [Read the docs](https:///docs.botui.org), see [examples](https:///examples.botui.org) or explore the code on !(github) [GitHub](https://github.com/moinism/botui)'
+    content: 'I dont care. I am awesome and you need to see it.'
   });
+  tutorial();
 };
 
 var ga_record = function(type, action) {
