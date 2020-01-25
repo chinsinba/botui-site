216bb7e1e9aa966fe452007f1fbba93c7941ac5b
syed <syed.mehtab@yara.com>
2020-01-25 20:18:28 +0800

    changed the whole flow with a share whatsapp button

--- assets/js/home-bot.js
+++ assets/js/home-bot.js
@@ -2,12 +2,12 @@
 var homeBot = BotUI('home-demo');
 
 homeBot.message.add({
-  content: 'Welcome to PLOTANA. I can analyse the plot you looking to buy/sell.'
+  content: 'Hi, I am Plotana.\n I analyze plots and help you invest your hard-earned money at a good location.'
 }).then(function () {
   return homeBot.message.add({
-    delay: 4000,
+    delay: 2000,
     loading: true,
-    content: 'Wanna see a demo of what I can do?'
+    content: 'Do you want to see what I can do?'
   });
 }).then(function () {
   return homeBot.action.button({
@@ -16,7 +16,7 @@ homeBot.message.add({
       text: 'Yes',
       value: 'sure'
     }, {
-      text: 'No, I dont believe you.',
+      text: 'No, I only believe myself.',
       value: 'skip'
     }]
   });
@@ -52,15 +52,15 @@ var tutorial = function () {
   }).then(function (res) {
     if(res.value == 'nothing') {
       return homeBot.message.add({
-        delay: 1500,
+        delay: 500,
         loading: true,
         content: 'You are not poor, you are lazy. Now tell me which location you would buy? '
       });
     }
     return homeBot.message.add({
-      delay: 1000,
+      delay: 500,
       loading: true,
-      content: 'Where is your plot?'
+      content: 'Where is the plot?'
     });
 
   }).then(function () {
@@ -76,30 +76,31 @@ var tutorial = function () {
     return homeBot.message.bot({
       delay: 1500,
       loading: true,
-      content: res.value + '. Aha, you are a rich fellow. Now please wait while I am pulling satellite images for analysis'
+      content: res.value + '. Aha, you are a rich fellow. Now please wait while I am pulling satellite images for analysis.'
     });
   }
   return homeBot.message.bot({
     delay: 1500,
     loading: true,
-    content: 'Smart Ass!! you changed the location huh!. You will only get analysis of BTM Layout for free!!. I am analysing BTM Layout'
+    content: 'Smart!! you changed the location huh!. For now, you will only get analysis of BTM Layout. Now please wait while I am pulling satellite images for analysis'
   });
   }).then(function (res) {
     return homeBot.message.add({
-      delay: 4000,
+      delay: 2000,
+      loading: true,
       type: 'embed',
       content: 'https://giphy.com/embed/HzMfJIkTZgx8s'
     
     });
   }).then(function (res) {
     return homeBot.message.bot({
-      delay: 4000,
+      delay: 2000,
       loading: true,
       content: 'Almost there.'
     });
   }).then(function (res) {
     return homeBot.message.bot({
-      delay: 4000,
+      delay: 2000,
       loading: true,
       content: 'Here you go!'
     });
@@ -107,21 +108,35 @@ var tutorial = function () {
     return homeBot.message.bot({
       delay: 500,
       loading: true,
-      content: 'I can see that in past 2 years 500 new homes have been built within 2 KM radius of your plot. Thats a good growth compared to Jaynagar area.'
+      content: 'I can see in past 2 years (from 2018 to 2019) 500 new homes have been constructed within 2 KM radius of your plot.'
     });
   }).then(function () {
     return homeBot.action.button({
       delay: 1000, 
       action: [{
         text: 'Tell me more!',
-        value: 'more'
+        value: 'tmore'
       }]
     });
   }).then(function (res) {
     return homeBot.message.bot({
-      delay: 5000,
+      delay: 2000,
       loading: true,
-      content: 'The area is densely populated and expected to grow. Further, I can see huge night light intensity, seems like people here are nocturnal. Also, there is very less tree cover. Its a concrete jungle!!'
+      content: 'The population within 2 kms has gone up from 120 people/sqKm in 2015 to 180people/sqKm. The location is growing very fast'
+    });
+  }).then(function () {
+    return homeBot.action.button({
+      delay: 1000,
+      action: [{
+        text: 'more',
+        value: 'more'
+      }]
+    });
+  }).then(function () {
+    return homeBot.message.bot({
+      delay: 2000,
+      loading: true,
+      content: 'The night lights intensity in last 2 years is growing fast. This means there is high economic activity happening in the location.'
     });
   }).then(function () {
     return homeBot.action.button({
@@ -131,15 +146,70 @@ var tutorial = function () {
         value: 'smore'
       }]
     });
-  }).then(function (res) {
+  }).then(function () {
     return homeBot.message.bot({
-      delay: 1000,
-      content: 'Thats it for now. I am working on comparative analytics, Slope and Flood analysis to suggest you a better plot and much more. Bookmark me sweetheart!.'
+      delay: 2000,
+      loading: true,
+      content: 'I can show other comparable locations for you to invest. But for that I want to know your budget?'
+    });
+  }).then(function () {
+    return homeBot.action.button({
+      delay: 200,
+      action: [{
+        text: '5-10 lakh',
+        value: '10l'
+      },
+      {text: '10-20 lakh',
+      value: '1020'
+    },{
+      text: '20-50 lakh',
+      value: '2050'
+    },{
+      text: 'I am very rich',
+      value: 'rich'
+    }]
     });
   }).then(function (res) {
+
+   if (res.value=== '10l')
+    {
     return homeBot.message.bot({
       delay: 1000,
-      content: 'Bye! 😒'
+      content: 'Your budget is too low for any good plot in Bangalore. You need to go and look for in the suburbs.'
+    });
+    }
+   if (res.value=== '1020')
+   {
+    return homeBot.message.bot({
+    delay: 1000,
+    content: 'coming soon!'
+   });
+   }
+   if (res.value=== '2050')
+   {
+    return homeBot.message.bot({
+    delay: 1000,
+    content: 'coming soon!'
+   });
+   }
+   if (res.value=== 'rich')
+   {
+    return homeBot.message.bot({
+    delay: 1000,
+    content: 'coming soon!'
+   });
+   }
+   }).then(function (res) {
+    return homeBot.message.bot({
+      delay: 1000,
+      content: 'Meanwhile, you can help me spread the word by introducing me to your family and friedns.'
+    });
+  }).then(function () {
+    ga_record('message', 'share');
+    return homeBot.message.bot({
+      delay: 1000,
+      type:'html',
+      content:'<a href="whatsapp://send?text=Hi, I am Plotana, I analyze plots. I am available at http://plotana.com " data-action="share/whatsapp/share"><img src="https://img.icons8.com/color/48/000000/whatsapp.png"></img></a>'
     });
   })
 };
@@ -149,7 +219,7 @@ var end = function () {
   homeBot.message.add({
     delay: 1000,
     loading: true,
-    content: 'Dumbass, I dont care. I am awesome and you need to see it.'
+    content: 'I dont care. I am awesome and you need to see it.'
   });
   tutorial();
 };
